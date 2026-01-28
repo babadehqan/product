c1b09c73d4b7c4c42038ea03a6e108963f62dc1d
babadehqan <baba.dehqan.afg@gmail.com>
2026-01-28 12:09:08 +0430

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const products = [
+/*const products = [
     {
         id: 1,
         name: "Afghan Momtaz Black Tea",
@@ -14,9 +14,38 @@ const products = [
         decription: "Natural Green tea for daily using"
     },
 ];
+*/
 
+const menu = document.getElementById("menu");
+const menuItem = document.getElementById("menuItems")
 
+menu.onclick =() => {
+    menuItem.style.display = 
+    menuItem.style.display === "flex" ? "none": "flex";
+};
 
+const image =[
+    "images/photo.jpg",
+    "images/photo.jpg",
+    "images/photo.jpg"
+];
+
+let index =0;
+setInterval(() => {
+    index = (index +1) % image.length;
+    document.getElementById("slider-img").src = image[index];
+},3000);
+
+const fades = document.querySelectorAll(".fade");
+window.addEventListener("scroll", () => {
+    fades.forEach(el => {
+        if(el.getBoundingClientRect().top < window.innerHeight -100){
+            el.classList.add("show");
+        }
+    })
+})
+
+/*
 window.onload = function(){
 const container = document.getElementById("product-list");
 
@@ -34,3 +63,4 @@ products.forEach(product => {
     container.appendChild(card);
 });
 };
+*/
