58adc8e0aeb0af99e31ef70faf3ea11563c84eb8
babadehqan <baba.dehqan.afg@gmail.com>
2026-01-28 09:54:35 +0430

    Add files via upload

--- script.js
+++ script.js
@@ -2,47 +2,17 @@ const products = [
     {
         id: 1,
         name: "Afghan Momtaz Black Tea",
-        price: "10$",
-        image:"images/pic1.jpg" ,
+        price: "6$",
+        image:"images/photo.jpg" ,
         decription: "Natural black tea for daily using"
     },
     {
         id: 2,
         name: "Afghan Momtaz Green Tea",
         price: "10$",
-        image: "images/pic.jpg" ,
+        image: "images/photo.jpg" ,
         decription: "Natural Green tea for daily using"
     },
-
-    {
-        id: 3,
-        name: "Afghan Momtaz Black Tea",
-        price: "10$",
-        image:"images/pic1.jpg" ,
-        decription: "Natural black tea for daily using"
-    },
-    {
-        id: 4,
-        name: "Afghan Momtaz Green Tea",
-        price: "10$",
-        image: "images/pic.jpg" ,
-        decription: "Natural Green tea for daily using"
-    },
-
-    {
-        id: 5,
-        name: "Afghan Momtaz Black Tea",
-        price: "10$",
-        image:"images/pic1.jpg" ,
-        decription: "Natural black tea for daily using"
-    },
-    {
-        id: 6,
-        name: "Afghan Momtaz Green Tea",
-        price: "10$",
-        image: "images/pic.jpg" ,
-        decription: "Natural Green tea for daily using"
-    }
 ];
 
 
